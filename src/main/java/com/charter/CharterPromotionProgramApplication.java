a857e45b51e3e6b38954144bed94deba27580009
Tony Tao <tonytao3@yahoo.ca>
2023-08-03 15:23:27 -0400

    Intial Project Structure

---
+++ src/main/java/com/charter/CharterPromotionProgramApplication.java
@@ -0,0 +1,13 @@
+package com.charter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharterPromotionProgramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CharterPromotionProgramApplication.class, args);
+	}
+
+}
